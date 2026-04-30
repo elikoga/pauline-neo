e17fccbb4c8aca12e7296ea1f94b80accd3736fd
Eli Kogan-Wang <elikowa@gmail.com>
2026-04-30 04:28:53 +0200

    Fix modal width: restore wide-screen styleWindow from main

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -76,7 +76,7 @@
 
 <Modal
   show={$modalStore}
-  styleWindow={{ backgroundColor: 'var(--background) !important' }}
+  styleWindow={{ backgroundColor: 'var(--background) !important', width: 'min(56rem, calc(100vw - 4rem))' }}
   classWindow="p-5"
   styleCloseButton={{ backgroundColor: 'var(--primary) !important' }}
   transitionBgProps={{ duration: 100 }}
